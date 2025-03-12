1425359a00d27051cadc46b92eadc8e29b301331
Diana Mendizabal <artedeamorperu@gmail.com>
2025-03-12 16:17:18 -0500

    Obtener valores del DOM

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var ListaNombres = [];
+
+var AgregarNombre = document.querySelector(".button-add");
+
+function agregar() {
+
+    var nombre = document.querySelector("#amigo").value;
+
+
+    console.log(nombre);
+
+
+};
+
+AgregarNombre.addEventListener("click", agregar);
+
+
+function agregarNombre () {
+
+
+
+
+};
+
+
